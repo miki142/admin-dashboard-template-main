0fc97831a0883875504893e0b170184213cfb13c
mikiyas hailu <mikiyashailushte@gmail.com>
2023-06-14 13:49:57 +0300

    admin panel

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const charts = document.querySelectorAll(".chart");
+
+charts.forEach(function (chart) {
+  var ctx = chart.getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".data-table").each(function (_, table) {
+    $(table).DataTable();
+  });
+});
+var chart_1 = document.getElementById('chart_1');
+var chart_2 = document.getElementById('chart_2');
+var chart_3 = document.getElementById('table_div');
+var table_btn = document.getElementById('table_btn');
+var chart_btn = document.getElementById('chart_btn');
+table_btn.addEventListener('click', () => {
+  chart_1.style.display ='none';
+  chart_2.style.display = 'none';
+  chart_3.style.display = 'block'; 
+} );
+chart_btn.addEventListener('click', () => {
+  chart_1.style.display ='block';
+  chart_2.style.display = 'block';
+  chart_3.style.display = 'none';
+})
